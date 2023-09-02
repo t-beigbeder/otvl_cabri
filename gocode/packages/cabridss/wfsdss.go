a0a442eb79914f72db3560d00dd74662da2271dd
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-09-02 22:26:07 +0200

    m

--- gocode/packages/cabridss/wfsdss.go
+++ gocode/packages/cabridss/wfsdss.go
@@ -1,18 +1,113 @@
 package cabridss
 
-type wfsDss struct {
-	apc    WebApiClient
-	closed bool
-}
+import (
+	"github.com/spf13/afero"
+	"io"
+)
 
 type WfsDssConfig struct {
 	DssBaseConfig
 	NoClientLimit bool
 }
 
-// NewWfsDss opens a web client for a "fsy" DSS (data storage system)
-// config provides the web client configuration
+type wfsDssImpl struct {
+	// like webDssImpl
+	apc WebApiClient
+}
+
+func (wdi *wfsDssImpl) Mkns(npath string, mtime int64, children []string, acl []ACLEntry) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) Updatens(npath string, mtime int64, children []string, acl []ACLEntry) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) Lsns(npath string) (children []string, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) IsDuplicate(ch string) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) GetContentWriter(npath string, mtime int64, acl []ACLEntry, cb WriteCloserCb) (io.WriteCloser, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) GetContentReader(npath string) (io.ReadCloser, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) Remove(npath string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) GetMeta(npath string, getCh bool) (IMeta, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) SetCurrentTime(time int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) SetMetaMockCbs(cbs *MetaMockCbs) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) SetAfs(tfs afero.Fs) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) GetAfs() afero.Fs {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) Close() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) SetSu() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (wdi *wfsDssImpl) SuEnableWrite(npath string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+// NewWfsDss opens a web client for a remote "fsy" DSS (data storage system)
+// wdc provides the web client configuration
 // returns a pointer to the ready to use DSS or an error if any occur
-func NewWfsDss(config WfsDssConfig, slsttime int64, aclusers []string) (Dss, error) {
-	panic("to be implemented NewWfsDss")
+func NewWfsDss(wdc WfsDssConfig) (Dss, error) {
+	wdi := &wfsDssImpl{}
+	var tlsConfig *TlsConfig
+	var err error
+	if wdc.WebProtocol == "https" {
+		tlsConfig = &TlsConfig{
+			cert:              wdc.TlsCert,
+			key:               wdc.TlsKey,
+			noClientCheck:     wdc.TlsNoCheck,
+			basicAuthUser:     wdc.BasicAuthUser,
+			basicAuthPassword: wdc.BasicAuthPassword,
+		}
+	}
+	remoteWdc := wdc
+	wdi.apc, err = NewWebApiClient(wdc.WebProtocol, wdc.WebHost, wdc.WebPort, tlsConfig, wdc.WebRoot, remoteWdc, wdc.WebClientTimeout)
+	_ = err
+	return wdi, nil
 }
