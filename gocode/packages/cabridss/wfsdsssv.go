d217a3b915e42f1b33848a7a81210dc22f5c8692
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-09-04 20:04:24 +0200

    mc

--- gocode/packages/cabridss/wfsdsssv.go
+++ gocode/packages/cabridss/wfsdsssv.go
@@ -1,25 +1,27 @@
 package cabridss
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func afsInitialize(dss Dss) error {
+	return nil
+}
+
+func sfsInitialize(c echo.Context) error {
+	dss := GetCustomConfig(c).(WebDssServerConfig).Dss
+	err := afsInitialize(dss)
+	if err != nil {
+		return NewServerErr("sfsInitialize", err)
+	}
+	return c.JSON(http.StatusOK, nil)
+}
 
 func WfsDssServerConfigurator(e *echo.Echo, root string, configs map[string]interface{}) error {
 	dss := configs[root].(WebDssServerConfig).Dss
 	_ = dss
-	e.GET(root+"initialize/:clId", sInitialize)
-	e.PUT(root+"recordClient/:clId", sRecordClient)
-	e.PUT(root+"updateClient/:clId", sUpdateClient)
-	e.POST(root+"queryMetaTimes", sQueryMetaTimes)
-	e.POST(root+"storeMeta", sStoreMeta)
-	e.DELETE(root+"removeMeta", sRemoveMeta)
-	e.DELETE(root+"xRemoveMeta", sXRemoveMeta)
-	e.POST(root+"pushContent", sPushContent)
-	e.POST(root+"loadMeta", sLoadMeta)
-	e.POST(root+"spGetContentReader", sSpGetContentReader)
-	e.GET(root+"queryContent/:ch", sQueryContent)
-	e.DELETE(root+"removeContent/:ch", sRemoveContent)
-	e.GET(root+"dumpIndex", sDumpIndex)
-	e.GET(root+"scanPhysicalStorage", sScanPhysicalStorage)
-	e.GET(root+"loadIndex", sLoadIndex)
+	e.GET(root+"wfsInitialize", sfsInitialize)
 	return nil
 }
 
