4f64fa4898cfb4a304389d00a31031ae5c50892a
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-09-09 17:55:43 +0200

    mc

--- gocode/packages/cabridss/wfsdsscl.go
+++ gocode/packages/cabridss/wfsdsscl.go
@@ -26,12 +26,19 @@ func cfsInitialize(apc WebApiClient) error {
 
 func cfsUpdatens(apc WebApiClient, npath string, mtime int64, children []string, acl []ACLEntry) error {
 	//wdc := wdi.apc.GetConfig().(WfsDssConfig)
+	var rer mError
 	_, err := apc.SimpleDoAsJson(http.MethodPost, apc.Url()+"wfsUpdatens",
 		mfsUpdateNs{
 			Npath:    npath,
 			Mtime:    mtime,
 			Children: children,
 			ACL:      acl,
-		}, nil)
-	return err
+		}, &rer)
+	if err != nil {
+		return fmt.Errorf("in cfsUpdatens: %w", err)
+	}
+	if rer.Error != "" {
+		return fmt.Errorf("in cfsUpdatens: %s", rer.Error)
+	}
+	return nil
 }
