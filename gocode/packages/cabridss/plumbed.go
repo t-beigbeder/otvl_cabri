643c0f7759045c56328c9dca81778e27d22edd4a
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-10-26 10:47:45 +0200

    removed useless lsns UI configuration for fsy -rc

--- gocode/packages/cabridss/plumbed.go
+++ gocode/packages/cabridss/plumbed.go
@@ -5,13 +5,6 @@ import "github.com/t-beigbeder/otvl_cabri/gocode/packages/plumber"
 func CabriPlumberDefaultConfig(serial bool, redLimit int) plumber.Config {
 	gm := uint(8)
 	sgm := uint(12)
-	//if redLimit != 0 {
-	//	gm = uint(redLimit / 10)
-	//	if gm < 1 {
-	//		gm = 1
-	//	}
-	//	sgm = gm
-	//}
 	return plumber.Config{
 		PlizerEnabled: !serial,
 		RglatorsByName: map[string]uint{
