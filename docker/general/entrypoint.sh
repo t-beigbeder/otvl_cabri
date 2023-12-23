aa4546befdca584b16a7513a89a63b92b623b539
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-12-23 19:13:22 +0100

    published 2 images on docker hub using github actions

---
+++ docker/general/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export HOME=$VHOME
+mkdir -p $VHOME || exit
+for f in /docker-entrypoint-init.d/*; do
+	case "$f" in
+		*.sh) echo "$0: running $f"; "$f" ;;
+		*)    echo "$0: ignoring $f" ;;
+	esac
+	echo
+done
+exec "$@"
